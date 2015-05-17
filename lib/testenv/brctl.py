bc776c360d9b0d9bfb58bad60058d1b45f1fdfc6
Dima Kuznetsov <dkuznets@redhat.com>
2015-05-17 14:52:14 +0300

    Add elaborate rules for testenv sudoers
    Change-Id: If87531d14e0312d61878f2941c8e347ad85facee
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>

--- lib/testenv/brctl.py
+++ lib/testenv/brctl.py
@@ -19,23 +19,30 @@
 #
 import utils
 
-BRCTL = 'brctl'
+_BRCTL = ['sudo', 'brctl']
+_IP = ['sudo', 'ip']
 
 
 def _brctl(command, *args):
-    ret, out, err = utils.run_command([BRCTL, command] + args)
+    ret, out, err = utils.run_command(_BRCTL + [command] + args)
     if ret:
         raise RuntimeError('brctl %s failed' % command)
     return ret, out, err
 
 
+def _name(name):
+    return 'te-%s' % name
+
+
 def _set_link(name, state):
-    ret, _, _ = utils.run_command(['ip', 'link', 'set', 'dev', name, state])
+    name = _name(name)
+    ret, _, _ = utils.run_command(_IP + ['link', 'set', 'dev', name, state])
     if ret:
         raise RuntimeError('Could not set %s to state %s' % (name, state))
 
 
 def create(name, stp=True):
+    name = _name(name)
     with utils.RollbackContext as rollback:
         _brctl('addbr', name)
         rollback.prependDefer(_brctl, 'delbr', name)
@@ -46,9 +53,11 @@ def create(name, stp=True):
 
 
 def destroy(name):
+    name = _name(name)
     _brctl('delbr', name)
 
 
 def exists(name):
+    name = _name(name)
     ret, out, err = _brctl('show', name)
     return err == ''
