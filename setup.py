b3169fff1b0a585654a38062127900e500ce4596
Dima Kuznetsov <dkuznets@redhat.com>
2015-05-17 11:19:50 +0300

    Fix PEP8 for setup.py
    Change-Id: I2855a52c1f9bac251b9353f907b03e983808014b
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ distutils.core.setup(
                 'contrib/ovirt/setup_scripts/setup_storage_iscsi.sh',
                 'contrib/ovirt/setup_scripts/setup_storage_nfs.sh',
                 'contrib/ovirt/setup_scripts/bz_1195882_libvirt_workaround.sh',
-                 'contrib/ovirt/setup_scripts/setup_container_host.sh',
+                'contrib/ovirt/setup_scripts/setup_container_host.sh',
             ],
         ),
         (
