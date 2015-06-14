106ef552c67bdef2d90867ea75d39d1eaf354801
Dima Kuznetsov <dkuznets@redhat.com>
2015-06-14 14:35:50 +0300

    Add automation build script
    Change-Id: I0e21f106253ea470c4061e2e4c380fa94a21f9db
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>

---
+++ automation/build-artifacts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+BUILDS=$PWD/automation-build/
+EXPORTS=$PWD/exported-artifacts/
+
+rm -rf $BUILDS $EXPORTS
+mkdir -p $BUILDS
+mkdir -p $EXPORTS/rpms
+
+make clean
+make srpm rpm OUTPUT_DIR=$BUILDS
+
+cp -av $BUILDS/dist $EXPORTS/
+cp -av $BUILDS/rpmbuild/SRPMS/ $EXPORTS/rpm/
+cp -av $BUILDS/rpmbuild/RPMS/ $EXPORTS/rpm/
---
+++ automation/check-patch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -ex
+make check-local
