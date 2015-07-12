2687fb25a9427592a367970e7c60b804e329ea69
David Caro <dcaroest@redhat.com>
2015-07-12 20:12:29 +0200

    Quoting some dangerous vars on build-artifacts
    Change-Id: Ic69e4f89b6c309cbc20b701ae1576b0b9e5e4419

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -2,13 +2,13 @@
 BUILDS=$PWD/automation-build/
 EXPORTS=$PWD/exported-artifacts/
 
-rm -rf $BUILDS $EXPORTS
-mkdir -p $BUILDS
-mkdir -p $EXPORTS/rpms
+rm -rf "$BUILDS" "$EXPORTS"
+mkdir -p "$BUILDS"
+mkdir -p "$EXPORTS/rpms"
 
 make clean
-make srpm rpm OUTPUT_DIR=$BUILDS
+make srpm rpm OUTPUT_DIR="$BUILDS"
 
-cp -av $BUILDS/dist $EXPORTS/
-cp -av $BUILDS/rpmbuild/SRPMS/ $EXPORTS/rpm/
-cp -av $BUILDS/rpmbuild/RPMS/ $EXPORTS/rpm/
+cp -av "$BUILDS/dist" "$EXPORTS/"
+cp -av "$BUILDS/rpmbuild/SRPMS/" "$EXPORTS/rpm/"
+cp -av "$BUILDS/rpmbuild/RPMS/" "$EXPORTS/rpm/"
