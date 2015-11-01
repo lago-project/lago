a808c0b63ef7d52465b7643550ffc07ad795c3bc
Tolik Litovsky <tlitovsk@redhat.com>
2015-11-01 16:40:36 +0200

    Adapting 3.5 tests
    Change-Id: I6412c4f86c1489826ac174afe6ea36ff21d01cc8
    Signed-off-by: Tolik Litovsky <tlitovsk@redhat.com>
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- contrib/ovirt/test_scenarios/bootstrap_3_5.py
+++ contrib/ovirt/test_scenarios/bootstrap_3_5.py
@@ -28,7 +28,7 @@ from ovirtlago import testlib
 # DC/Cluster
 DC_NAME = 'test-dc'
 DC_VER_MAJ = 3
-DC_VER_MIN = 6
+DC_VER_MIN = 5
 CLUSTER_NAME = 'test-cluster'
 CLUSTER_CPU_FAMILY = 'Intel Conroe Family'
 
@@ -69,8 +69,8 @@ def add_dc(api):
 def add_cluster(api):
     p = params.Cluster(
         name=CLUSTER_NAME,
-        cpu=params.Cpu(
-            type_=CLUSTER_CPU_FAMILY,
+        cpu=params.CPU(
+            id=CLUSTER_CPU_FAMILY,
         ),
         version=params.Version(
             major=DC_VER_MAJ,
