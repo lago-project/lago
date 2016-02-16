5cb35608a905f74484f40eb231f21455df62ab20
David Caro <dcaroest@redhat.com>
2016-02-16 09:49:58 +0100

    Changing also the check-merged to check deps
    Change-Id: Ie21fff16aed1c92de0ff52dbd4d26d41b54c4e28
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -3,36 +3,70 @@ EXPORTED_DIR="$PWD/exported-artifacts"
 DOCS_DIR="$EXPORTED_DIR/docs"
 
 
+die() {
+    echo "$@"
+    exit 1
+}
+
+
 [[ -d "$EXPORTED_DIR" ]] || mkdir -p "$EXPORTED_DIR"
 
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo '~*          Building docs                              ~'
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 rm -rf "$DOCS_DIR"
-pip install -r docs/requires.txt
+rm -rf tests/docs_venv
+virtualenv -q tests/docs_venv
+source tests/docs_venv/bin/activate
+pip --quiet install --requirement docs/requires.txt
 make docs
+deactivate
 mv docs/_build "$DOCS_DIR"
 
-# Style and unit test
-# required for the formatting
-pip install yapf
+# Style and unit tests, using venv to make sure the installation tests pull in
+# all the dependencies
+rm -rf tests/venv
+# the system packages are needed for python-libguestfs
+virtualenv -q --system-site-packages tests/venv
+source tests/venv/bin/activate
+pip --quiet install --requirement test-requires.txt
+export PYTHONPATH
 make check-local
+deactivate
 
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo '~*          Running build/installation tests           ~'
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 automation/build-artifacts.sh
+echo "Installing..."
+if hash dnf &>/dev/null; then
+    YUM=dnf
+else
+    YUM=yum
+fi
 # to allow negated groups in globs
 shopt -s extglob
 # fail if a glob turns out empty
 shopt -s failglob
-echo "Installing..."
-if hash dnf &>/dev/null; then
-    dnf install -y exported-artifacts/!(*.src).rpm
-else
-    yum install -y exported-artifacts/!(*.src).rpm
-fi
+for package in {python-,}lago {python-,}lago-ovirt; do
+    echo "    $package: installing"
+    ## Install one by one to make sure the deps are ok
+    $YUM install -y exported-artifacts/"$package"-[[:digit:]]!(*src).rpm \
+    && echo "    $package: OK" \
+    || die "    $package: FAILED"
+    if [[ "$package" == "lago" ]]; then
+        echo "    Checking that lago imports are not missing"
+        lago -h > /dev/null \
+        && echo "    OK" \
+        || die "    FAILED"
 
+    elif [[ "$package" == "lago-ovirt" ]]; then
+        echo "    Checking that lago ovirt imports are not missing"
+        lago ovirt -h > /dev/null \
+        && echo "    OK" \
+        || die "    FAILED"
+    fi
+done
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo '~*          Running functional tests                   ~'
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
@@ -43,6 +77,24 @@ fi
 bats tests/functional \
 | tee exported-artifacts/functional_tests.tap
 res=${PIPESTATUS[0]}
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~*          Generating html report                     ~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+cat  >exported-artifacts/index.html <<EOR
+<html>
+<body>
+<ul>
+<li>
+  <a href="docs/html/index.html">Docs page</a>
+</li>
+<li>
+  <a href="basic_functional_tests.tap">TAP tests result</a>
+</li>
+</ul
+</body>
+</html>
+EOR
+echo "~ Report at file://$PWD/exported-artifacts/index.html  ~"
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 exit "$res"
