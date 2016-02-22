0835e3b0d821bf9f9363ddc84ed191a390e985e7
Yaniv Kaul <ykaul@redhat.com>
2016-02-22 14:42:48 +0200

    Add explicit logging to the action of collecting artificat from each VM

--- contrib/ovirt/lib/ovirtlago/__init__.py
+++ contrib/ovirt/lib/ovirtlago/__init__.py
@@ -524,7 +524,7 @@ class OvirtPrefix(lago.Prefix):
         os.makedirs(output_dir)
 
         def _collect_artifacts(vm):
-            with LogTask('%s' % vm.name()):
+            with LogTask('Collecting artificats from VM %s' % vm.name()):
                 path = os.path.join(output_dir, vm.name())
                 os.makedirs(path)
                 vm.collect_artifacts(path)
