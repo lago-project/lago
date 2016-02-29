20b7a386cf0b0153d9a847327d2f5f5cc18fe799
David Caro <dcaroest@redhat.com>
2016-02-29 12:57:54 +0100

    Moved some confs to setup.cfg
    Change-Id: I005548bd81fbbad1e09c03fb3968d3259dd92145
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- setup.py
+++ setup.py
@@ -8,52 +8,14 @@ setup(
         'Deploy and tear down environments of several virtual machines'
     ),
     license='GNU GPLv2+',
-    author='Dima Kuznetsov',
-    author_email='dkuznets@redhat.com',
-    url='redhat.com',
-    package_dir={
-        'lago': 'lago',
-        'lago_template_repo': 'lago_template_repo',
-        'ovirtlago': 'ovirtlago'
-    },
+    author='David Caro',
+    author_email='dcaro@redhat.com',
+    url='http://lago.readthedocs.com',
     packages=[
         'lago',
         'lago.plugins',
         'lago_template_repo',
         'ovirtlago',
     ],
-    package_data={
-        'lago': [
-            '*.xml',
-            '*.log.conf',
-        ],
-    },
     provides=['lago', 'ovirtlago'],
-    entry_points={
-        'console_scripts': [
-            'lagocli=lago.cmd:main',
-            'lago=lago.cmd:main',
-        ],
-        'lago.plugins.cli': [
-            'init=lago.cmd:do_init',
-            'cleanup=lago.cmd:do_cleanup',
-            'destroy=lago.cmd:do_destroy',
-            'start=lago.cmd:do_start',
-            'status=lago.cmd:do_status',
-            'stop=lago.cmd:do_stop',
-            'shell=lago.cmd:do_shell',
-            'console=lago.cmd:do_console',
-            'snapshot=lago.cmd:do_snapshot',
-            'revert=lago.cmd:do_revert',
-            'copy-from-vm=lago.cmd:do_copy_from_vm',
-            'copy-to-vm=lago.cmd:do_copy_to_vm',
-            'ovirt=ovirtlago.cmd:OvirtCLI',
-            'template-repo=lago_template_repo:TemplateRepoCLI',
-        ],
-        'lago.plugins.output': [
-            'default=lago.plugins.output:DefaultOutFormatPlugin',
-            'json=lago.plugins.output:JSONOutFormatPlugin',
-            'yaml=lago.plugins.output:YAMLOutFormatPlugin',
-        ],
-    },
 )
