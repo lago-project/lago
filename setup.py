056ce6075faa26d3b9d54c20e9232ec788bd79f3
David Caro <dcaroest@redhat.com>
2016-02-29 13:16:42 +0100

    Allow python setup * commands to work
    Without requiring any special env vars or anything
    
    Change-Id: Ic33d1d38657ae2f41f29ed03c4ecfb32457f6752
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,62 @@
 import os
+import subprocess
 from setuptools import setup
+
+
+def check_output(args):
+    proc = subprocess.Popen(
+        args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout, stderr = proc.communicate()
+
+    if proc.returncode:
+        raise RuntimeError(
+            'Failed to run %s\nrc=%s\nstdout=\n%sstderr=%s' %
+            (args, proc.returncode, stdout, stderr)
+        )
+
+    return stdout
+
+
+def get_version():
+    """
+    Retrieves the version of the package, from the PKG-INFO file or generates
+    it with the version script
+    Returns:
+        str: Version for the package
+    Raises:
+        RuntimeError: If the version could not be retrieved
+    """
+    if 'LAGO_VERSION' in os.environ and os.environ['LAGO_VERSION']:
+        return os.environ['LAGO_VERSION']
+
+    version = None
+    if os.path.exists('PKG-INFO'):
+        with open('PKG-INFO') as info_fd:
+            for line in info_fd.readlines():
+                if line.startswith('Version: '):
+                    version = line.split(' ', 1)[-1]
+
+    elif os.path.exists('scripts/version_manager.py'):
+        version = check_output(
+            ['scripts/version_manager.py', '.', 'version']
+        ).strip()
+
+    if version is None:
+        raise RuntimeError('Failed to get package version')
+
+    # py3 compatibility step
+    if not isinstance(version, str) and isinstance(version, bytes):
+        version = version.decode()
+
+    return version
+
 
 setup(
     name='lago',
-    version=os.environ['LAGO_VERSION'],
+    version=get_version(),
     description=(
         'Deploy and tear down environments of several virtual machines'
     ),
