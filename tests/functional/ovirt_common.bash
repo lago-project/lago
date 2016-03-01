24ff36d13d83f472c454ad16a3df6c3c1b5980eb
David Caro <dcaroest@redhat.com>
2016-03-01 18:46:36 +0100

    Added ovirt_common lib
    Change-Id: Ibeb484e48f66e4a758d481b9f5ae94c7b2651736

---
+++ tests/functional/ovirt_common.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+OVIRT_VERBS=(
+    collect
+    deploy
+    engine-setup
+    reposetup
+    revert
+    runtest
+    serve
+    snapshot
+    start
+    stop
+)
+
+OVIRTCLI=(
+    lagocli
+    ovirt
+)
+
