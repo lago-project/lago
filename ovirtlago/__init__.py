40db66f29131462c3a39856c52fb7b58c244353a
David Caro <dcaroest@redhat.com>
2016-05-26 13:59:28 +0200

    Use only-missing to avoid overriding custom rpms
    Change-Id: I4cdd0baeb8e578e2c34bc7a5245b90ac042e2bf5
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- ovirtlago/__init__.py
+++ ovirtlago/__init__.py
@@ -223,14 +223,14 @@ class OvirtPrefix(Prefix):
 
             rpm_dirs.extend(
                 [
-                    os.path.join(folder, dist)
+                    os.path.join(folder, dist) + ':only-missing'
                     for folder in project_roots if os.path.exists(folder)
                 ]
             )
 
             rpm_dirs.extend(
                 [
-                    os.path.join(repos_path, name)
+                    os.path.join(repos_path, name) + ':only-missing'
                     for name in repo_names if name.endswith(dist)
                 ],
             )
